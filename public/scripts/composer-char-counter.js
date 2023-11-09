9d217d7d878eacc6fe1dd397f0b16517e11301f6
Idil Mahamed <mahamed.idil@gmail.com>
2023-11-09 13:28:05 -0500

    created and updated composer-char-counter.js,now complete

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  console.log("DOM is ready!"); 
+  const length = 140;
+  $('.new-tweet textarea').on('input',function() {
+    const textLength = $(this).val().length;
+    const remainingLength = length - textLength;
+    const counterElement = $('output.counter');
+    console.log(counterElement)
+    counterElement.text(remainingLength);
+    if (remainingLength < 0) {
+      counterElement.addClass('overLimit');
+    } else {
+      counterElement.removeClass('overLimit');
+    }
+  });
 });
+  
 
-console.log("Character counter script loaded!")
+
